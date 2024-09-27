90353cdcdffb1ee4a34d84171c739042c02c0771
huayitang <tanghuayi906@outlook.com>
2024-09-27 13:14:41 +0200

    debug

--- backend/dj_backend/settings.py
+++ backend/dj_backend/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
+# Initialize environment variables
+env = environ.Env(
+    # Set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
